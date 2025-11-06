5f345313a64d3af117d27e54452a0727bf226e45
전재석 <darkiss2355@gmail.com>
2025-11-06 18:01:39 +0900

    프로그램 컨트롤러 추가
    전체 리스트 가져오기

--- src/main/java/com/semicolon/backend/domain/program/service/ProgramService.java
+++ src/main/java/com/semicolon/backend/domain/program/service/ProgramService.java
@@ -3,9 +3,12 @@ package com.semicolon.backend.domain.program.service;
 import com.semicolon.backend.domain.program.dto.ProgramDTO;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional
 public interface ProgramService {
     public ProgramDTO getOne(long pno);
     public Long register(ProgramDTO program);
+    public List<ProgramDTO> getList();
 
 }
