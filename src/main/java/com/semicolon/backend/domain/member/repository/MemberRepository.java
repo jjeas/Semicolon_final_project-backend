477c612dffb3600f58f2f38ee7568787c61fc1c6
Shinyoung <dkssud3524@naver.com>
2025-11-13 12:05:44 +0900

    MyPage Partner 신청 폼 생성
    구현 완료

--- src/main/java/com/semicolon/backend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/semicolon/backend/domain/member/repository/MemberRepository.java
@@ -1,8 +1,21 @@
 package com.semicolon.backend.domain.member.repository;
 
 import com.semicolon.backend.domain.member.entity.Member;
+import com.semicolon.backend.domain.partner.dto.PartnerDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("""
+    select new com.semicolon.backend.domain.partner.dto.PartnerDTO(p.status)
+    from Partner p
+    join p.member m
+    where m.memberId = :memId
+""")
+    Optional<PartnerDTO> getPartnerStatus(@Param("memId") Long memId);
 
 }
--- src/main/java/com/semicolon/backend/domain/member/service/MemberService.java
+++ src/main/java/com/semicolon/backend/domain/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.semicolon.backend.domain.member.service;
 
 import com.semicolon.backend.domain.member.dto.MemberDTO;
+import com.semicolon.backend.domain.member.entity.Member;
+import com.semicolon.backend.domain.partner.dto.PartnerDTO;
+
+import java.util.Optional;
 
 public interface MemberService {
     public MemberDTO getOne(Long memberId);
     public void register(MemberDTO memberDTO);
+    public PartnerDTO getPartnerStatus(Long memberId);
 }
