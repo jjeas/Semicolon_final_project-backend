6290168163a85f4d964781260c427398bc5ec99d
Kho23 <wndrn2002@naver.com>
2025-12-01 21:12:18 +0900

    251201 수강신청 로직 변경
    long 타입 id 받기 -> 로그인된 유저 loginId 받기

--- src/main/java/com/semicolon/backend/domain/registration/service/RegistrationService.java
+++ src/main/java/com/semicolon/backend/domain/registration/service/RegistrationService.java
@@ -1,13 +1,10 @@
 package com.semicolon.backend.domain.registration.service;
 
-import com.semicolon.backend.domain.member.entity.Member;
 import com.semicolon.backend.domain.registration.dto.RegistrationDTO;
-import com.semicolon.backend.domain.registration.entity.Registration;
-
 import java.util.List;
 
 public interface RegistrationService {
-    public void register(Long registrationId, String loginId);
-    public void cancel(String loginId, Long registrationId);
-    public List<RegistrationDTO> getList(String loginId);
+     void register(Long registrationId, String loginId);
+     void cancel(String loginId, Long registrationId);
+     List<RegistrationDTO> getList(String loginId);
 }
