0974e6bab10428d741a77cfacd583cdb7241059b
Kho23 <wndrn2002@naver.com>
2025-12-09 17:53:58 +0900

    201209 이건호 채팅 로직 구현중

--- src/main/java/com/semicolon/backend/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/semicolon/backend/domain/chat/repository/ChatRepository.java
@@ -8,8 +8,7 @@ import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat,Long> {
 
-    @Query("select c.roomId from Chat c group by c.roomId order by MAX(c.sendAt) DESC") //최신 메세지가 온 순서대로 방 번호만 가져오기
-    List<Long> findRoomList();
-
+    @Query("select c from Chat c where (c.sendAt, c.roomId) in (select Max(sub.sendAt), sub.roomId from Chat sub group by sub.roomId) order by c.sendAt desc") //최신 메세지가 온 순서대로 방 번호만 가져오기
+    List<Chat> findRoomList();
     List<Chat> findByRoomIdOrderBySendAtAsc(Long id);
 }
