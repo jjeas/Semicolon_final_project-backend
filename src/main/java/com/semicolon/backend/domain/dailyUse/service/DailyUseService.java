e88db36c14939bb31676f95b206f5843dccf93bd
전재석 <darkiss2355@gmail.com>
2025-12-09 18:24:57 +0900

    일일이용예약
    일일이용예약 조회& 가격 추가 & 삭제 기능 추가

--- src/main/java/com/semicolon/backend/domain/dailyUse/service/DailyUseService.java
+++ src/main/java/com/semicolon/backend/domain/dailyUse/service/DailyUseService.java
@@ -3,6 +3,10 @@ package com.semicolon.backend.domain.dailyUse.service;
 import com.semicolon.backend.domain.dailyUse.dto.DailyUseDTO;
 import com.semicolon.backend.domain.dailyUse.entity.DailyUse;
 
+import java.util.List;
+
 public interface DailyUseService {
     public DailyUse register(String loginIdFromToken, DailyUseDTO dto);
+    public List<DailyUseDTO> getList(String loginIdFromToken);
+    public void delete(Long id);
 }
